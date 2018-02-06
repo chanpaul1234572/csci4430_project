634368d01503069368585043cf8f7a770cee98e8
Parallel -chanpaul <Chan Paul>
2018-02-06 22:03:17 +0800

    prepare_request coded

--- client.c
+++ client.c
@@ -7,11 +7,33 @@
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include<message.h>
+
+void request_prepare(struct message* message_to_send, char* cmd, char* file, int payload_size){
+	const int header_size = 15;
+	strcpy(message_to_send -> protocol , "myftp");
+	if(strcmp("list", cmd) == 0){
+		message_to_send -> type = 0xA1;
+		message_to_send -> length = header_size;
+	}
+	else if(strcmp("get", cmd) == 0){
+		message_to_send -> type = 0xB1;
+		message_to_send -> length = header_size + payload_size;
+	}
+	else if(strcmp("PUT", cmd) == 0){
+		message_to_send -> type = 0xC1;
+		messgae_to_send -> length = header_size + payload_size;
+	}
+}
 
 
 int main(int argc, char** argv){
 		const int port = atoi(argv[2]);
 		int sd = socket(AF_INET, SOCK_STREAM, 0);
+		char pathname[100] = void;
+		strcat("./", pathname);
+		strcat(pathname, argv[4]);
+		int fd = open(pathname, O_CREAT);
 		char* ipstr = argv[1];
 		struct sockaddr_in server_addr;
 		if (sd < 0){
@@ -20,11 +42,14 @@ int main(int argc, char** argv){
 		memset(&server_addr, 0, sizeof(server_addr));
 		server_addr.sin_family = AF_INET;
 		server_addr.sin_addr.s_addr=inet_addr(ipstr);
-		server_addr.sin.port = htons(port);
+		server_addr.sin_port = htons(port);
 		if(connect(sd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0){
        		printf("connection error: %s (Errno:%d)\n", strerror(errno), errno);
       		exit(0);
 		}
-		printf("connected to the server: %s\n", inet_ntoa(server_addr.sin_addr.s_addr));
-		
+		printf("connected to the server: %s\n", inet_ntoa((server_addr.sin_addr.s_addr)));
+	return 0;
+}		
+
+
 
--- message.h
+++ message.h
@@ -1,9 +1,10 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-struct message_s{
+typedef struct message_s{
     unsigned char protocol[5];          //protocol string(MYFTP)
     unsigned char type;                 //type (1bytes)
     unsigned int length;                //length (header + payload) (4 bytes)
-}__attribute__((packed));
+} message __attribute__((packed));
+
 
