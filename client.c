8813d7d3d69d97efbcddcd1a5d1779c189604f9b
Parallel -chanpaul <Chan Paul>
2018-02-06 21:00:28 +0800

    message.h added

--- client.c
+++ client.c
@@ -10,11 +10,21 @@
 
 
 int main(int argc, char** argv){
-		
+		const int port = atoi(argv[2]);
 		int sd = socket(AF_INET, SOCK_STREAM, 0);
+		char* ipstr = argv[1];
 		struct sockaddr_in server_addr;
-		if (s < 0){
+		if (sd < 0){
 				errexit("Can not create socket :%s\n", strerror(errno));
 		}
 		memset(&server_addr, 0, sizeof(server_addr));
-		server_addr.
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_addr.s_addr=inet_addr(ipstr);
+		server_addr.sin.port = htons(port);
+		if(connect(sd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0){
+       		printf("connection error: %s (Errno:%d)\n", strerror(errno), errno);
+      		exit(0);
+		}
+		printf("connected to the server: %s\n", inet_ntoa(server_addr.sin_addr.s_addr));
+		
+
---
+++ message.h
@@ -0,0 +1,9 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+struct message_s{
+    unsigned char protocol[5];          //protocol string(MYFTP)
+    unsigned char type;                 //type (1bytes)
+    unsigned int length;                //length (header + payload) (4 bytes)
+}__attribute__((packed));
+
