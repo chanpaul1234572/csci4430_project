6158251385e1d4f3cdafc90125336959b788147f
1155083040 <1155083040@link.cuhk.edu.hk>
2018-02-07 20:21:37 +0800

    thread added & fork->clientconnection

--- server.c
+++ server.c
@@ -17,6 +17,42 @@
 ->listen() max pending N ...?
 */
 
+void *workerthread(int *clientsd_p){
+	int client_sd =(int*)clientsd_p; 
+	free(clientsd_p);
+    pthread_detach(pthread_self());
+
+	clientconnection(client_sd); //handlle client connection 
+
+	return(NULL);
+}
+
+//Handling client requests
+void clientconnection(int client_sd){
+
+	if((len = recv(client_sd,buf,sizeof(buf),0)) < 0){
+		printf("Receive Error!");
+	}else{
+		if(buf->type == 0xA1){
+				//list
+			//working..
+		}
+		else
+			if(buf->type == 0xB1){
+					//get
+			}
+		else
+			if(buf->type == 0xC1){
+					//put
+			}
+		else
+			printf("Wrong request!");
+	}
+	pthread_exit(NULL);
+	close(sd);
+	return 0;
+}
+
 int main(int argc,char** argv){
 
 	unsigned short port;
@@ -26,7 +62,8 @@ int main(int argc,char** argv){
 		exit(1);
 	}	
 	port = atoi(argv[1]); //port num receive 
-	pthread_t 
+	pthread_t threadid; 
+	int *clientsd_p; //clinet socket descriptor pointer
 	int sd = socket(AF_INET, SOCK_STREAM, 0);
 	int client_sd, buf; 
 	struct sockaddr_in server_addr, client_addr;
@@ -51,37 +88,20 @@ int main(int argc,char** argv){
 				printf("Accept error: %s (Errno:%d)\n", strerror(errno), errno);
 				exit(0);
 		}else{
-			
-
-		}
-	}
-
-// fork version
-// need put back to suitable space
-   if(!fork()){
-		close(sd);
-		if((len = recv(client_sd,buf,sizeof(buf),0)) < 0){
-			printf("Receive Error!");
-		}else{
-			if(buf->type == 0xA1){
-					//list
-				//working..
+			//open worker thread
+			clientsd_p = malloc(sizeof(int client_sd)); 
+			clientsd_p = client_sd;
+			if(pthread_create(&threadid, NULL, workerthread, clientsd_p)==-1){
+				printf("create thread error: %s (Errno:%d)\n", strerror(errno), errno);
+				free(clientsd_p);
+				free(client_addr);
+				close(sd);
+				close(client_sd);
+				pthread_exit(NULL);
 			}
-			else
-				if(buf->type == 0xB1){
-						//get
-				}
-			else
-				if(buf->type == 0xC1){
-						//put
-				}
-			else
-				printf("Wrong request!");
-
 		}
-
 	}
-
 	close(sd);
 	return 0;
+	//End of server process
 }
