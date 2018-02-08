57362bea56d9de395409bf6f2af8a7646fa2c442
chanpaul1234572 <chanpaul1234572@gmail.com>
2018-02-08 16:26:28 +0800

    correct some syntax error

--- myftp.h
+++ myftp.h
@@ -1,10 +1,11 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
-typedef struct message_s{
+struct message_s{
     unsigned char protocol[5];          //protocol string(MYFTP)
     unsigned char type;                 //type (1bytes)
     unsigned int length;                //length (header + payload) (4 bytes)
-} message __attribute__((packed));
+} __attribute__((packed));
 
-
+typedef struct message_s message;
+#endif
